5d00ffb21eedc8cf71b6de0663a897b7ad60855e
Tony Cardella <cardella@gmail.com>
2023-12-05 06:58:05 -0600

    Day 2 part 2 WIP

--- UnitTests/Day05.cs
+++ UnitTests/Day05.cs
@@ -19,8 +19,15 @@ public class Day05
         for (var i = 0; i < inputs.Length; i++)
         {
             if (inputs[i].StartsWith("seeds:"))
-                almanac.Seeds = inputs[i].Split(':').Last().Split(' ').Where(e => !string.IsNullOrWhiteSpace(e))
-                    .Select(long.Parse);
+            {
+                var rawSeeds = inputs[i].Split(':').Last().Split(' ').Where(e => !string.IsNullOrWhiteSpace(e))
+                    .Select(long.Parse).ToList();
+
+                for (int j = 0; j < rawSeeds.Count(); j += 2)
+                {
+                    almanac.Seeds.Add(new(rawSeeds[j], 1));
+                }
+            }
 
             if (inputs[i].StartsWith("seed-to-soil map:"))
             {
@@ -118,7 +125,9 @@ public class Day05
 
         foreach (var seed in almanac.Seeds)
         {
-            var soil = almanac.Seed2SoilMap.From(seed);
+            for (long i = seed.Item1; i < seed.Item1 + seed.Item2; i++)
+            {
+                var soil = almanac.Seed2SoilMap.From(i);
             var fertilizer = almanac.Soil2FertilizerMap.From(soil);
             var water = almanac.Fertilizer2WaterMap.From(fertilizer);
             var light = almanac.Water2LightMap.From(water);
@@ -128,6 +137,7 @@ public class Day05
 
             if (location < actual)
                 actual = location;
+            }
         }
 
         actual.Should().Be(expected);
@@ -139,16 +149,144 @@ public class Day05
     }
 
     [Theory]
-    [InlineData("Input/05/example.txt", 30)]
+    [InlineData("Input/05/example.txt", 46)]
     [InlineData("Input/05/input.txt", 5747443)]
     public async Task Part2(string inputFilePath, int expected)
     {
         var inputs = await File.ReadAllLinesAsync(inputFilePath);
+
+        var almanac = new Almanac();
+
+        for (var i = 0; i < inputs.Length; i++)
+        {
+            if (inputs[i].StartsWith("seeds:"))
+            {
+                var rawSeeds = inputs[i].Split(':').Last().Split(' ').Where(e => !string.IsNullOrWhiteSpace(e))
+                    .Select(long.Parse).ToList();
+
+                for (int j = 0; j < rawSeeds.Count(); j+=2)
+                {
+                    almanac.Seeds.Add(new(rawSeeds[j], rawSeeds[j + 1]));
+                }
+            }
+
+            if (inputs[i].StartsWith("seed-to-soil map:"))
+            {
+                i += 1;
+                while (i < inputs.Length && !string.IsNullOrWhiteSpace(inputs[i]))
+                {
+                    var x = inputs[i].Split(' ').Where(e => !string.IsNullOrWhiteSpace(e)).Select(long.Parse).ToArray();
+                    var y = Parse(x);
+                    almanac.Seed2SoilMap.Add(y.source, y.destination, y.n);
+
+                    i++;
+                }
+            }
+
+            if (inputs[i].StartsWith("soil-to-fertilizer map:"))
+            {
+                i += 1;
+                while (i < inputs.Length && !string.IsNullOrWhiteSpace(inputs[i]))
+                {
+                    var x = inputs[i].Split(' ').Where(e => !string.IsNullOrWhiteSpace(e)).Select(long.Parse).ToArray();
+                    var y = Parse(x);
+                    almanac.Soil2FertilizerMap.Add(y.source, y.destination, y.n);
+
+                    i++;
+                }
+            }
+
+            if (inputs[i].StartsWith("fertilizer-to-water map:"))
+            {
+                i += 1;
+                while (i < inputs.Length && !string.IsNullOrWhiteSpace(inputs[i]))
+                {
+                    var x = inputs[i].Split(' ').Where(e => !string.IsNullOrWhiteSpace(e)).Select(long.Parse).ToArray();
+                    var y = Parse(x);
+                    almanac.Fertilizer2WaterMap.Add(y.source, y.destination, y.n);
+
+                    i++;
+                }
+            }
+
+            if (inputs[i].StartsWith("water-to-light map:"))
+            {
+                i += 1;
+                while (i < inputs.Length && !string.IsNullOrWhiteSpace(inputs[i]))
+                {
+                    var x = inputs[i].Split(' ').Where(e => !string.IsNullOrWhiteSpace(e)).Select(long.Parse).ToArray();
+                    var y = Parse(x);
+                    almanac.Water2LightMap.Add(y.source, y.destination, y.n);
+
+                    i++;
+                }
+            }
+
+            if (inputs[i].StartsWith("light-to-temperature map:"))
+            {
+                i += 1;
+                while (i < inputs.Length && !string.IsNullOrWhiteSpace(inputs[i]))
+                {
+                    var x = inputs[i].Split(' ').Where(e => !string.IsNullOrWhiteSpace(e)).Select(long.Parse).ToArray();
+                    var y = Parse(x);
+                    almanac.Light2TemperatureMap.Add(y.source, y.destination, y.n);
+
+                    i++;
+                }
+            }
+
+            if (inputs[i].StartsWith("temperature-to-humidity map:"))
+            {
+                i += 1;
+                while (i < inputs.Length && !string.IsNullOrWhiteSpace(inputs[i]))
+                {
+                    var x = inputs[i].Split(' ').Where(e => !string.IsNullOrWhiteSpace(e)).Select(long.Parse).ToArray();
+                    var y = Parse(x);
+                    almanac.Temperature2HumidityMap.Add(y.source, y.destination, y.n);
+
+                    i++;
+                }
+            }
+
+            if (inputs[i].StartsWith("humidity-to-location map:"))
+            {
+                i += 1;
+                while (i < inputs.Length && !string.IsNullOrWhiteSpace(inputs[i]))
+                {
+                    var x = inputs[i].Split(' ').Where(e => !string.IsNullOrWhiteSpace(e)).Select(long.Parse).ToArray();
+                    var y = Parse(x);
+                    almanac.Humidity2LocationMap.Add(y.source, y.destination, y.n);
+
+                    i++;
+                }
+            }
+        }
+
+        var actual = long.MaxValue;
+
+        foreach (var seed in almanac.Seeds)
+        {
+            for (var i = seed.Item1; i < seed.Item1 + seed.Item2; i++)
+            {
+                var soil = almanac.Seed2SoilMap.From(i);
+                var fertilizer = almanac.Soil2FertilizerMap.From(soil);
+                var water = almanac.Fertilizer2WaterMap.From(fertilizer);
+                var light = almanac.Water2LightMap.From(water);
+                var temperature = almanac.Light2TemperatureMap.From(light);
+                var humidity = almanac.Temperature2HumidityMap.From(temperature);
+                var location = almanac.Humidity2LocationMap.From(humidity);
+
+                if (location < actual)
+                    actual = location;
+            }
+        }
+
+        actual.Should().Be(expected);
     }
 
     public class Almanac
     {
-        public IEnumerable<long> Seeds { get; set; }
+        public List<(long, long)> Seeds { get; set; } = new();
         public Map Seed2SoilMap { get; set; } = new();
         public Map Soil2FertilizerMap { get; set; } = new();
         public Map Fertilizer2WaterMap { get; set; } = new();
