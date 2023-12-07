13be4eb291ee548f2ca9798b44cdad585e7d4166
Tony Cardella <cardella@gmail.com>
2023-12-07 08:50:37 -0600

    Oops...missed the refactor part.

--- UnitTests/Day07.cs
+++ UnitTests/Day07.cs
@@ -1,5 +1,4 @@
 ﻿using FluentAssertions;
-using Xunit.Abstractions;
 
 namespace UnitTests;
 
@@ -8,16 +7,10 @@ namespace UnitTests;
 /// </summary>
 public class Day07
 {
-    private readonly ITestOutputHelper _testOutputHelper;
-
-    private readonly Dictionary<string, int> _handRanks = new[] { "HighCard", "1Pair", "2Pair", "3OfAKind", "FullHouse", "4OfAKind", "5OfAKind" }
-        .Select((e, i) => new { Type = e, Rank = i })
-        .ToDictionary(e => e.Type, e => e.Rank);
-
-    public Day07(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
+    private readonly Dictionary<string, int> _handRanks =
+        new[] { "HighCard", "1Pair", "2Pair", "3OfAKind", "FullHouse", "4OfAKind", "5OfAKind" }
+            .Select((e, i) => new { Type = e, Rank = i })
+            .ToDictionary(e => e.Type, e => e.Rank);
 
     [Theory]
     [InlineData("Input/07/example.txt", 6440)]
@@ -30,47 +23,48 @@ public class Day07
             .Select(f => new Hand { Cards = f[0], Bid = int.Parse(f[1]) })
             .ToList();
 
-        var scorer = new CardStrengthFinder();
-        var identifier = new HandIdentifier();
-        var handComparer = new HandComparer();
+        var cardStrengths = new[] { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' }
+            .Reverse()
+            .Select((e, i) => new { Label = e, Strength = i })
+            .ToDictionary(e => e.Label, e => e.Strength);
+        var utilities = new HandUtilities(cardStrengths);
+        var comparer = new HandComparer();
 
-        var actual = hands.Select(h => identifier.IdentifyPart1(h))
+        var actual = hands
+            .Select(h => h with { Strengths = utilities.Score(h) })
+            .Select(h => h with { Type = utilities.Identify(h) })
             .Select(h => h with { Rank = _handRanks[h.Type] })
-            .Order(handComparer)
+            .Order(comparer)
             .Select((e, i) => e.Bid * (i + 1))
             .Sum();
-        
+
         actual.Should().Be(expected);
     }
 
     [Theory]
     [InlineData("Input/07/example.txt", 5905)]
-    [InlineData("Input/07/input.txt", 34123437)]
+    [InlineData("Input/07/input.txt", 248256639)]
     public async Task Part2(string inputFilePath, int expected)
     {
         var inputs = await File.ReadAllLinesAsync(inputFilePath);
 
         var hands = inputs.Select(e => e.Split(' '))
-            .Select(f => new Hand2 { Cards = f[0], Bid = int.Parse(f[1]) })
+            .Select(f => new Hand { Cards = f[0], Bid = int.Parse(f[1]) })
             .ToList();
 
-        var strengthFinder = new CardStrengthFinder();
-        var identifier = new HandIdentifier();
-        var handComparer = new HandComparer2();
+        var cardStrengths = new[] { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' }
+            .Reverse()
+            .Select((e, i) => new { Label = e, Strength = i })
+            .ToDictionary(e => e.Label, e => e.Strength);
+        var utilities = new HandUtilities(cardStrengths);
+        var comparer = new HandComparer();
 
-        var preActual = hands.Select(h => h with { CardStrengths = strengthFinder.Score(h) })
-            .Select(h=> h with {Cards = identifier.DoIt(h)})
-            .Select(h => h with { Type = identifier.IdentifyPart2(h) })
+        var actual = hands
+            .Select(h => h with { Strengths = utilities.Score(h) })
+            .Select(h => h with { Cards = utilities.ApplyJRule(h) })
+            .Select(h => h with { Type = utilities.Identify(h) })
             .Select(h => h with { Rank = _handRanks[h.Type] })
-            .Order(handComparer);
-
-        foreach (var e in preActual)
-        {
-            _testOutputHelper.WriteLine(e.Cards + "   " + e.Type + "   " + string.Join(',', e.CardStrengths) + "   " + e.Bid);
-        }
-        
-
-        var actual = preActual
+            .Order(comparer)
             .Select((e, i) => e.Bid * (i + 1))
             .Sum();
 
@@ -87,34 +81,28 @@ public class Day07
     [InlineData("23456", "HighCard")]
     public void HandIdentifier_HappyPathTest(string cards, string expected)
     {
-        var sut = new HandIdentifier();
+        var sut = new HandUtilities(new Dictionary<char, int>());
 
-        var hand = new Hand()
+        var hand = new Hand
         {
             Cards = cards
         };
 
-        sut.IdentifyPart1(hand).Type.Should().Be(expected);
+        sut.Identify(hand).Should().Be(expected);
     }
 
     [Fact]
-    public void DoIt()
+    public void ApplyJRule_HappyPathTest()
     {
-        var sut = new HandIdentifier();
+        var sut = new HandUtilities(new[] { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' }
+            .Reverse()
+            .Select((e, i) => new { Label = e, Strength = i })
+            .ToDictionary(e => e.Label, e => e.Strength));
 
-var actual =        sut.DoIt(new Hand2() { Cards = "JJ354" });
+        var actual = sut.ApplyJRule(new Hand { Cards = "JJ354" });
 
-actual.Should().Be("55354");
+        actual.Should().Be("55354");
     }
-}
-
-public class CardStrengthFinder
-{
-    private readonly Dictionary<char, int> _cardStrengths = new[] { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' }.Reverse()
-        .Select((e, i) => new { Label = e, Strength = i })
-        .ToDictionary(e => e.Label, e => e.Strength);
-
-    public int[] Score(Hand2 hand) => hand.Cards.Select(c => _cardStrengths[c]).ToArray();
 }
 
 public record Hand
@@ -123,20 +111,11 @@ public record Hand
     public string Type { get; set; }
     public int Bid { get; set; }
     public int Rank { get; set; }
-    public int Index { get; set; }
+    public int[] Strengths { get; set; }
 }
 
-public record Hand2 : Hand
+public class HandComparer() : IComparer<Hand>
 {
-    public int[] CardStrengths { get; set; }
-}
-
-public class HandComparer : IComparer<Hand>
-{
-    private readonly Dictionary<char, int> _cardStrengths = new[] { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' }.Reverse()
-        .Select((e, i) => new { Label = e, Strength = i })
-        .ToDictionary(e => e.Label, e => e.Strength);
-
     #region Implementation of IComparer<in Hand>
 
     public int Compare(Hand? x, Hand? y)
@@ -148,17 +127,10 @@ public class HandComparer : IComparer<Hand>
         {
             for (var i = 0; i < x.Cards.Length; i++)
             {
-                var a = _cardStrengths[ x.Cards[i]];
-                var b = _cardStrengths[y.Cards[i]];
-
-                if (a > b)
-                    return 1;
-
-                if (a == b)
+                var temp = x.Strengths[i].CompareTo(y.Strengths[i]);
+                if (temp == 0)
                     continue;
-
-                if (a < b)
-                    return -1;
+                return temp;
             }
 
             return 0;
@@ -170,66 +142,37 @@ public class HandComparer : IComparer<Hand>
     #endregion
 }
 
-public class HandComparer2 : IComparer<Hand2>
+public class HandUtilities(Dictionary<char, int> cardStrengths)
 {
-    #region Implementation of IComparer<in Hand>
+    public int[] Score(Hand hand) => hand.Cards.Select(c => cardStrengths[c]).ToArray();
 
-    public int Compare(Hand2? x, Hand2? y)
+    public string Identify(Hand hand)
     {
-        if (x.Rank > y.Rank)
-            return 1;
-
-        if (x.Rank == y.Rank)
-        {
-            for (var i = 0; i < x.CardStrengths.Length; i++)
-            {
-                var temp = x.CardStrengths[i].CompareTo(y.CardStrengths[i]);
-                if (temp != 0)
-                    return temp;
-            }
-
-            return 0;
-        }
-
-        return -1;
-    }
-
-    #endregion
-}
-
-public class HandIdentifier
-{
-    public Hand IdentifyPart1(Hand hand)
-    {
-        var groupings = from e in hand.Cards
+        var characters = (from e in hand.Cards
             group e by e
             into g
             select new
             {
                 Character = g.Key,
                 Count = g.Count()
-            };
+            }).ToArray();
 
-        hand.Type = groupings.Count() switch
+        return characters.Length switch
         {
             5 => "HighCard",
             4 => "1Pair",
-            3 => groupings.Any(e => e.Count == 3)
+            3 => characters.Any(e => e.Count == 3)
                 ? "3OfAKind"
                 : "2Pair",
-            2 => groupings.Any(e => e.Count == 4)
+            2 => characters.Any(e => e.Count == 4)
                 ? "4OfAKind"
                 : "FullHouse",
             1 => "5OfAKind",
             _ => throw new ArgumentOutOfRangeException()
         };
-
-        return hand;
     }
-    private readonly Dictionary<char, int> _cardStrengths = new[] { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' }.Reverse()
-        .Select((e, i) => new { Label = e, Strength = i })
-        .ToDictionary(e => e.Label, e => e.Strength);
-    public string DoIt(Hand2 hand)
+
+    public string ApplyJRule(Hand hand)
     {
         if (!hand.Cards.Contains('J'))
             return hand.Cards;
@@ -237,43 +180,19 @@ public class HandIdentifier
         if (hand.Cards == "JJJJJ")
             return "AAAAA";
 
-        var groupings = from e in hand.Cards
-            group e by e
-            into g
-            select new
-            {
-                Character = g.Key,
-                Count = g.Count(),
+        var mostCommonChar = from e in from e in hand.Cards
+                group e by e
+                into g
+                select new
+                {
+                    Character = g.Key,
+                    Count = g.Count(),
+                    Score = cardStrengths[g.Key]
+                }
+            where e.Character != 'J'
+            orderby e.Count, e.Score
+            select e.Character;
 
-            };
-
-        var mostCommonChar = groupings.Where(e => e.Character != 'J').MaxBy(e => e.Count)!.Character;
-        return hand.Cards.Replace('J', mostCommonChar);
-    }
-
-    public string IdentifyPart2(Hand2 hand)
-    {
-        var groupings = from e in hand.Cards
-            group e by e
-            into g
-            select new
-            {
-                Character = g.Key,
-                Count = g.Count()
-            };
-
-        return groupings.Count() switch
-        {
-            5 => "HighCard",
-            4 => "1Pair",
-            3 => groupings.Any(e => e.Count == 3)
-                ? "3OfAKind"
-                : "2Pair",
-            2 => groupings.Any(e => e.Count == 4)
-                ? "4OfAKind"
-                : "FullHouse",
-            1 => "5OfAKind",
-            _ => throw new ArgumentOutOfRangeException()
-        };
+        return hand.Cards.Replace('J', mostCommonChar.Last());
     }
 }
