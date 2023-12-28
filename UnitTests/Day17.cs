2b7170bff2001d09c63a61704a416c46fd9e08c1
Tony Cardella <cardella@gmail.com>
2023-12-28 13:23:26 -0600

    Day 17 WIP

---
+++ UnitTests/Day17.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+
+namespace UnitTests;
+
+/// <summary>
+///     Prompt: https://adventofcode.com/2023/day/17
+/// </summary>
+public class Day17
+{
+    [Theory]
+    [InlineData("Input/17/example.txt", 102)]
+    //[InlineData("Input/17/input.txt", 511498)]
+    public async Task Part1(string inputFilePath, int expected)
+    {
+        var inputs = await File.ReadAllLinesAsync(inputFilePath);
+
+        //char[][] map = inputs.Select(input => input.ToCharArray()).ToArray();
+
+        //      var start = new Point(1, 0);
+
+        //// TODO: Implement djikstra's algorithm
+        
+    }
+
+    //[Theory]
+    //[InlineData("Input/17/example1.txt", 145)]
+    //[InlineData("Input/17/input.txt", 284674)]
+    public async Task Part2(string inputFilePath, int expected)
+    {
+        var inputs = await File.ReadAllLinesAsync(inputFilePath);
+
+    }
+}
